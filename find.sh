75378f1507ee93089ee12aef628ff63131938c70
willandru <willandru@hotmail.com>
2021-12-04 14:53:46 -0500

    xgrap command

--- find.sh
+++ find.sh
@@ -10,3 +10,10 @@
 							#indiques el grupo asignado:: de forma atabulada dime el modo. 
 							
 	find . -type f -printf "%f\t%p\t%u\t%g\t%m\n" | column -t   # Con column para que se vea mas bonito :ł
+	
+	find -name file.name				# Muestra un archivo en especifoco, se debe poner el nombre commpleto con la extension del archivo, Este 								comando devuelve la ruta del archivo
+	
+	
+	find . -type f | xargs cat  			#XARGS -COMMAND permite aplicar a un comando a el output retornado por otro comando; En este ejemplo se hace 								un cat de todos los archivos encontrados en el directorio. XARGS esta bastante bien para formatear OUTPUTS
+	
+	find . -type f | xargs grep "text"		#Para aplicar un grep de una palabra encontrada dentro de algun archivo
